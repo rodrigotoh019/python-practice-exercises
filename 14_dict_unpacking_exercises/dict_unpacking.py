80ade117ab831ac634bff0d69a9736a2085a1a6d
Rodrigo Toh <rodrigotoh019@gmail.com>
2025-10-23 05:01:41 +0800

    Add .items() unpacking practice

---
+++ 14_dict_unpacking_exercises/dict_unpacking.py
@@ -0,0 +1,19 @@
+students = {
+    "Carl": [11,70,29,90],
+    "Pen": [80,90,22,59],
+    "Charlie": [88, 87, 91, 90]
+}
+
+# for name, scores in students.items():
+#         print(name," -", *scores)
+
+# for name, scores in students.items():
+#     print(name)
+#     for score in scores:
+#         print(" -", score)
+
+# for name, scores in students.items():
+#     print(f"{name}, {sum(scores)/len(scores)}")
+
+# student_list = [(name, sum(scores)/len(scores)) for name, scores in students.items()]
+# print(student_list)
