bba19b86cb6d002593f5bbc4694979326dbf425e
Rodrigo Toh <rodrigotoh019@gmail.com>
2025-10-24 20:11:19 +0800

    Added comment on each for loop unpacking.

--- 14_dict_unpacking_exercises/dict_unpacking.py
+++ 14_dict_unpacking_exercises/dict_unpacking.py
@@ -5,15 +5,15 @@ students = {
 }
 
 # for name, scores in students.items():
-#         print(name," -", *scores)
+#         print(name, *scores)              # printing with * to unpack individual value inside scores for each name
 
-# for name, scores in students.items():
+# for name, scores in students.items():     # printing the key, path with some formatting
 #     print(name)
-#     for score in scores:
-#         print(" -", score)
+#     for score in scores:                  # for each name, unpack the scores further into individual value
+#         print(" -", score)                # " -" is there to have some format on scores when printing
 
 # for name, scores in students.items():
-#     print(f"{name}, {sum(scores)/len(scores)}")
+#     print(f"{name}, {sum(scores)/len(scores)}")   # getting the average score of each students
 
-# student_list = [(name, sum(scores)/len(scores)) for name, scores in students.items()]
+# student_list = [(name, sum(scores)/len(scores)) for name, scores in students.items()]     # list comprehension of the average score of students
 # print(student_list)
